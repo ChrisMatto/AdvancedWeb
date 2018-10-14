e4bb9f0093d72515a8f47c72291b5fcdc92d5a89
ChrisMatto <christian-toni@hotmail.it>
2018-10-14 12:22:16 +0200

    BIG ONE

---
+++ AdvancedWebTest/out/artifacts/AdvancedWebTest_war_exploded/js/homeMaker.js
@@ -0,0 +1,53 @@
+function homeMaker(){
+    $.ajax({
+        url:'http://localhost:8084/AdvancedWeb/rest/home/cdl',
+        dataType: 'json',
+        type:'GET',
+        success: function(json){
+            //console.log(json);
+            //console.log(json[0]);
+
+            /*for(var i=0;i<1;i++){
+                $('#cdlink').attr('href','OnClick()');
+                $('#cdimg').attr('src',json[i].immagine);
+                if(json[i].nome_it.trim())
+                    $('#nome').text(json[i].nome_it);
+                else
+                    $('#nome').text(json[i].nome_en);
+                if(json[i].descrizione_it.trim())
+                    $('#descrizione').html(json[i].descrizione_it);
+                else
+                    $('#descrizione').html(json[i].descrizione_en);
+                $('#cfu').text(json[i].cfu+' CFU');
+                $('#cdlink2').attr('href','OnClick()');
+            }*/
+            
+            //var template=$('#cdl').wrap('<p/>').parent().html();
+            //$('#cdl').unwrap();
+            var template=$('#cdl').html();
+            //console.log(template);
+            var newTemp=Mustache.render(template,{'cdl':json});
+            $('#cdl').replaceWith(newTemp);
+            
+            $.ajax({
+                url:'http://localhost:8084/AdvancedWeb/rest/home/cdlm',
+                dataType: 'json',
+                type:'GET',
+                success: function(json){
+                    var template=$('#cdlm').html();
+                    var newTemp=Mustache.render(template,{'cdlm':json});
+                    $('#cdlm').replaceWith(newTemp);
+                }
+            });
+        }
+    });
+    /*var home={};
+    $.get('http://localhost:8084/AdvancedWeb/rest/cdl','json',function(data){home['cdl']=data;});
+    $.get('http://localhost:8084/AdvancedWeb/rest/cdlm','json',function(data){home['cdlm']=data;});
+    console.log(home);
+    var template=$('#home').html();
+    var newTemp=Mustache.render(template,home);
+    $('#home').replaceWith(newTemp);*/
+    
+    
+}
---
+++ AdvancedWebTest/src/Classi/DescrizioneIt.java
@@ -0,0 +1,141 @@
+package Classi;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Table(name = "descrizione_it", schema = "advancedweb", catalog = "")
+public class DescrizioneIt {
+    private int corso;
+    private String prerequisiti;
+    private String obiettivi;
+    private String modEsame;
+    private String modInsegnamento;
+    private String sillabo;
+    private String note;
+    private String homepage;
+    private String forum;
+    private String risorseExt;
+
+    @Id
+    @Column(name = "Corso")
+    public int getCorso() {
+        return corso;
+    }
+
+    public void setCorso(int corso) {
+        this.corso = corso;
+    }
+
+    @Basic
+    @Column(name = "Prerequisiti")
+    public String getPrerequisiti() {
+        return prerequisiti;
+    }
+
+    public void setPrerequisiti(String prerequisiti) {
+        this.prerequisiti = prerequisiti;
+    }
+
+    @Basic
+    @Column(name = "Obiettivi")
+    public String getObiettivi() {
+        return obiettivi;
+    }
+
+    public void setObiettivi(String obiettivi) {
+        this.obiettivi = obiettivi;
+    }
+
+    @Basic
+    @Column(name = "Mod_Esame")
+    public String getModEsame() {
+        return modEsame;
+    }
+
+    public void setModEsame(String modEsame) {
+        this.modEsame = modEsame;
+    }
+
+    @Basic
+    @Column(name = "Mod_Insegnamento")
+    public String getModInsegnamento() {
+        return modInsegnamento;
+    }
+
+    public void setModInsegnamento(String modInsegnamento) {
+        this.modInsegnamento = modInsegnamento;
+    }
+
+    @Basic
+    @Column(name = "Sillabo")
+    public String getSillabo() {
+        return sillabo;
+    }
+
+    public void setSillabo(String sillabo) {
+        this.sillabo = sillabo;
+    }
+
+    @Basic
+    @Column(name = "Note")
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+
+    @Basic
+    @Column(name = "Homepage")
+    public String getHomepage() {
+        return homepage;
+    }
+
+    public void setHomepage(String homepage) {
+        this.homepage = homepage;
+    }
+
+    @Basic
+    @Column(name = "Forum")
+    public String getForum() {
+        return forum;
+    }
+
+    public void setForum(String forum) {
+        this.forum = forum;
+    }
+
+    @Basic
+    @Column(name = "Risorse_ext")
+    public String getRisorseExt() {
+        return risorseExt;
+    }
+
+    public void setRisorseExt(String risorseExt) {
+        this.risorseExt = risorseExt;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        DescrizioneIt that = (DescrizioneIt) o;
+        return corso == that.corso &&
+                Objects.equals(prerequisiti, that.prerequisiti) &&
+                Objects.equals(obiettivi, that.obiettivi) &&
+                Objects.equals(modEsame, that.modEsame) &&
+                Objects.equals(modInsegnamento, that.modInsegnamento) &&
+                Objects.equals(sillabo, that.sillabo) &&
+                Objects.equals(note, that.note) &&
+                Objects.equals(homepage, that.homepage) &&
+                Objects.equals(forum, that.forum) &&
+                Objects.equals(risorseExt, that.risorseExt);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(corso, prerequisiti, obiettivi, modEsame, modInsegnamento, sillabo, note, homepage, forum, risorseExt);
+    }
+}
