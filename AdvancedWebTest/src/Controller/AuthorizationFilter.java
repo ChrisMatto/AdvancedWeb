fafd6dd6a22650877a94c3f4d7f33a3f49b8f733
ChrisMatto <christian-toni@hotmail.it>
2018-10-22 16:52:52 +0200

    fix

--- AdvancedWebTest/src/Controller/AuthorizationFilter.java
+++ AdvancedWebTest/src/Controller/AuthorizationFilter.java
@@ -7,7 +7,6 @@ import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerRequestFilter;
 import javax.ws.rs.container.PreMatching;
 import javax.ws.rs.core.Response;
-import java.io.IOException;
 import java.net.URI;
 
 @PreMatching
