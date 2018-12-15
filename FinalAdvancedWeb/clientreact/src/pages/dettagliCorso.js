673e4507fe9b5fc45885275d268a7bb95245404c
ChrisMatto <christian-toni@hotmail.it>
2018-12-15 16:49:17 +0100

    inizio render dettagli corso

--- FinalAdvancedWeb/clientreact/src/pages/dettagliCorso.js
+++ FinalAdvancedWeb/clientreact/src/pages/dettagliCorso.js
@@ -61,6 +61,71 @@ export default class DettagliCorso extends Component {
     }
 
     render() {
-        return <div></div>
+        var infoBase = this.state.infoBase;
+        var descrizione = this.state.corso.descrizioneIt;
+        var propedeudici = [];
+        var mutuati = [];
+        var moduli = [];
+        var mutuato = <span>Mutuato da:</span>;
+        var cdlList = [];
+        return (
+            <section>
+                <div className="container">
+                <div className="row">
+                    <div className="col-md-12 text-center">
+                        <h1>Dettagli Del Corso</h1>
+                        <p className="lead"></p>
+                    </div>
+                </div>
+                <div className="row" id="sub-header-features-2">
+                    <div className="col-md-4">
+                        <h2>Informazioni generali</h2>
+                        <ul className="list_ok">
+                            <li><strong>SSD:</strong> &nbsp;{infoBase.ssd}</li>
+                            <li><strong>CDL:</strong>&nbsp;{cdlList}</li>
+                            <li><strong>Lingua:</strong>&nbsp;{infoBase.lingua}</li>
+                            <li><strong>Semestre:</strong>&nbsp;{infoBase.semestre}</li>
+                            <li><strong>CFU (e tipo):</strong>&nbsp;  {infoBase.cfu} / {infoBase.tipologia}</li>
+                            <li><strong>Anno Accademico:</strong>&nbsp; {infoBase.anno} / {infoBase.anno + 1}</li>
+                        </ul>
+                    </div>
+
+                    <div className="col-md-4">
+                        <h2>Prerequisiti</h2>
+                        <span>{descrizione.prerequisiti}</span>
+                    </div>
+
+                <div className="col-md-4">
+                    <h2>Obiettivi</h2>
+                    <span>{descrizione.obiettivi}</span>
+            </div>
+
+                        
+                </div>
+
+                <div id="divider"></div>
+                <div class="row">
+                    <div class="col-md-12" id="tipocorso">
+
+
+                            {propedeudici}
+
+
+                            {mutuati}   
+                            
+                            {mutuato}
+                            
+                        
+                            {moduli}
+
+                        
+
+                    </div>
+                </div>
+
+
+            </div>
+            </section>
+        );
     }
 }
