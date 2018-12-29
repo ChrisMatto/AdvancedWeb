8fb5bc698efd0107bccfae6dd75ab205842807e8
Unknown <christian-toni@hotmail.it>
2018-12-29 18:31:53 +0100

    lista cdl nel corso + refactoring short if else

--- FinalAdvancedWeb/clientreact/src/pages/dettagliCorso.js
+++ FinalAdvancedWeb/clientreact/src/pages/dettagliCorso.js
@@ -61,6 +61,7 @@ export default class DettagliCorso extends Component {
     }
 
     render() {
+        var lingua = this.props.lingua;
         var infoBase = this.state.infoBase;
         var descrizioneIt = this.state.corso.descrizioneIt;
         if (descrizioneIt === undefined)
@@ -75,41 +76,49 @@ export default class DettagliCorso extends Component {
         var propedeudici = [];
         var mutuati = [];
         var moduli = [];
-        var mutuato = <span>{this.props.lingua === "it" ? 'Mutuato Da' : 'Borrowed By'}:</span>;
-        var cdlList = [];
+        var mutuato = [];
+        var cdlList = this.state.corso.cdl;
+        var cdlString = null;
+
+        var abbrCdl;
+        for (let cdl in cdlList)
+        {
+            abbrCdl = lingua === 'it' ? cdlList[cdl].abbrIt || cdlList[cdl].abbrEn : cdlList[cdl].abbrEn || cdlList[cdl].abbrIt;
+            cdlString = cdlString ? cdlString + ', ' + abbrCdl : abbrCdl;
+        }
         return (
             <section id = "sub-header">
                 <div className="container">
                     <div className="row">
                         <div className="col-md-12 text-center">
-                            <h1>{this.props.lingua === "it" ? "Dettagli Del Corso" : "Course Details"}</h1>
+                            <h1>{lingua === "it" ? "Dettagli Del Corso" : "Course Details"}</h1>
                             <p className="lead"></p>
                         </div>
                     </div>
                     <div className="row" id="sub-header-features-2">
                         <div className="col-md-4">
-                            <h2>{this.props.lingua === "it" ? 'Informazioni Generali' : 'General Informations'}</h2>
+                            <h2>{lingua === "it" ? 'Informazioni Generali' : 'General Informations'}</h2>
                             <ul className="list_ok">
                                 <li><strong>SSD:</strong> &nbsp;{infoBase.ssd}</li>
-                                <li><strong>CDL:</strong>&nbsp;{cdlList}</li>
-                                <li><strong>{this.props.lingua === "it" ? 'Lingua' : 'Language'}:</strong>&nbsp;{infoBase.lingua}</li>
-                                <li><strong>{this.props.lingua === "it" ? 'Semestre' : 'Semester'}:</strong>&nbsp;{infoBase.semestre}</li>
-                                <li><strong>CFU ({this.props.lingua === "it" ? 'e tipo' : 'and type'}):</strong>&nbsp;  {infoBase.cfu} / {infoBase.tipologia}</li>
-                                <li><strong>{this.props.lingua === "it" ? 'Anno Accademico' : 'Accademic Year'}:</strong>&nbsp; {infoBase.anno} / {infoBase.anno + 1}</li>
+                                <li><strong>CDL:</strong>&nbsp;{cdlString}</li>
+                                <li><strong>{lingua === "it" ? 'Lingua' : 'Language'}:</strong>&nbsp;{infoBase.lingua}</li>
+                                <li><strong>{lingua === "it" ? 'Semestre' : 'Semester'}:</strong>&nbsp;{infoBase.semestre}</li>
+                                <li><strong>CFU ({lingua === "it" ? 'e tipo' : 'and type'}):</strong>&nbsp;  {infoBase.cfu} / {infoBase.tipologia}</li>
+                                <li><strong>{lingua === "it" ? 'Anno Accademico' : 'Accademic Year'}:</strong>&nbsp; {infoBase.anno} / {infoBase.anno + 1}</li>
                             </ul>
                         </div>
 
                         <div className="col-md-4">
-                            <h2>{this.props.lingua === "it" ? 'Prerequisiti' : 'Prerequisites'}</h2>
+                            <h2>{lingua === "it" ? 'Prerequisiti' : 'Prerequisites'}</h2>
                             <span dangerouslySetInnerHTML = {{
-                                __html: this.props.lingua === 'it' ? descrizioneIt.prerequisiti ? descrizioneIt.prerequisiti : descrizioneEn.prerequisiti : descrizioneEn.prerequisiti ? descrizioneEn.prerequisiti : descrizioneIt.prerequisiti
+                                __html: lingua === 'it' ? descrizioneIt.prerequisiti || descrizioneEn.prerequisiti : descrizioneEn.prerequisiti || descrizioneIt.prerequisiti
                                 }}/>
                         </div>
 
                         <div className="col-md-4">
-                            <h2>{this.props.lingua === "it" ? 'Obiettivi' : 'Objectives'}</h2>
+                            <h2>{lingua === "it" ? 'Obiettivi' : 'Objectives'}</h2>
                             <span dangerouslySetInnerHTML = {{
-                                __html: this.props.lingua === 'it' ? descrizioneIt.obiettivi ? descrizioneIt.obiettivi : descrizioneEn.obiettivi : descrizioneEn.obiettivi ? descrizioneEn.obiettivi : descrizioneIt.obiettivi
+                                __html: lingua === 'it' ? descrizioneIt.obiettivi || descrizioneEn.obiettivi : descrizioneEn.obiettivi || descrizioneIt.obiettivi
                                 }}/>
                         </div>
 
