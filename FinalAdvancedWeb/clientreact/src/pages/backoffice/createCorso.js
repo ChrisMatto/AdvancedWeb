95acfc3ebf149a5d0ded664754f09e586e21ee53
ChrisMatto <christian-toni@hotmail.it>
2019-02-02 21:26:13 +0100

    start semantic e progress bar

--- FinalAdvancedWeb/clientreact/src/pages/backoffice/createCorso.js
+++ FinalAdvancedWeb/clientreact/src/pages/backoffice/createCorso.js
@@ -1,17 +1,21 @@
 import React, { Component, Fragment } from 'react';
 import { Link, Redirect } from 'react-router-dom';
+import { Segment, Header, Progress } from 'semantic-ui-react';
 
 export default class CreateCorso extends Component {
     constructor() {
         super();
         this.state = {
-
+            
         };
     }
 
     render() {
         return (
-            <div className = 'col-md-8' style = {{ backgroundColor: 'red' }}>dsd</div>
+            <Segment className = 'col-md-8' color = 'teal'>
+                <Progress value='1' total='5'/>
+                <Header size='medium'>Crea un Nuovo Corso</Header>
+            </Segment>
         );
     }
 
